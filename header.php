34974532d6a351297753a56761576206fbb9d5f4
dipeshs13 <dipeshshresthaa00@gmail.com>
2025-05-17 21:33:00 +0545

    first commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<nav id="navbar">
+    <div id="navbar-inner">
+        <a class="brand" href="#">Research Paper</a>
+        <div class="nav-container">
+            <ul class="nav-navbar">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">PDF</a></li>
+                <li><a href="#" class="login-link">Login</a></li>
+                <li><a href="#" class="register-link">Register</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ancizar+Serif:ital,wght@0,300..900;1,300..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <section class="main-content">
+        <div class="main-container">
+            <div class="main-text">
+                <h3 class="main-heading">RESEARCH PAPERS</h3>
+                <span class="small-heading">in one place</span>
+                <p class="main-description">Easily manage, upload and access academic papers in seconds.<br>Our system
+                    simplifies organization for students, researchers, and admins.
+                
+                </p>
+                
+            </div>
+        </div>
+        <div class="main-image">
+        <img src="images/pdf1.jpg" alt="" srcset="" >
+    </section>
+
+    <section id="search-section">
+        <h3>Search Here</h3>
+        <div class="search-container">
+            <form action="#" method="get">
+                <input type="text" name="query" id="search-input" placeholder="Search for papers...">
+                <button type="submit" id="search-button">Search</button>
+            </form>
+        </div>
+    </section>
+        
+    </section>
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
